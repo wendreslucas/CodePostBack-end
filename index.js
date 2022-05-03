44ab80b46666a905aa0f2f6233ea2ccc351e024a
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-05-03 16:37:00 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3333
+
+const routes = require('./src/routes')
+const connectToDataBase = require('./src/database')
+const cors = require('cors')
+const product = require('./api/product')
+
+connectToDataBase()
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/product', product)
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`Backend started at http://localhost:${PORT}`)
+})
