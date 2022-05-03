8cf05f55de8891a0f3a60323968db106d620cf99
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-05-03 16:09:13 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-// require('dotenv').config({ path: '.env' })
-
 const express = require('express')
 const app = express()
 const routes = require('./routes')
@@ -9,13 +7,13 @@ const cors = require('cors')
 const product = require('../api/product')
 
 connectToDataBase()
-const port = 3333
+const PORT = process.env.PORT || 3333
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/product', product)
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`Backend started at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Backend started at http://localhost:${PORT}`)
 })
