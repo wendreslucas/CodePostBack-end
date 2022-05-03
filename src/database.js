32c1e601fc007009df4d519ab19e09cbd3dfefe4
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-05-03 15:10:16 -0300

    ajustes

--- src/database.js
+++ src/database.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 
 function connectToDataBase() {
--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
+// require('dotenv').config({ path: '.env' })
 
+const express = require('express')
 const app = express()
 const routes = require('./routes')
+
 const connectToDataBase = require('./database')
 const cors = require('cors')
 const product = require('../api/product')
 
 connectToDataBase()
+const port = 3333
 
-app.use(express.json({ extended: false }))
+app.use(cors())
+app.use(express.json())
 app.use('/api/product', product)
 app.use(routes)
 
-const port = process.env.port || 3333
 app.listen(port, () => {
   console.log(`Backend started at http://localhost:${port}`)
 })
